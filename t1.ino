84a35eff19807ab5a72d33ed138eb2912a4364a8
mrodriguezarias <14845585+mrodriguezarias@users.noreply.github.com>
2019-12-07 14:25:18 -0300

    Update t1.ino

--- t1.ino
+++ t1.ino
@@ -10,14 +10,17 @@ int pingTriggerPin = 39;
 int pingVccPin = 41;
 int distanceThreshold = 10;
 int turnDelay = 250;
-int testMode = 0;
+
+int normalMode = 0;
+int testMode = 1;
+int operationMode = normalMode;
 
 int speed0 = 0;
-int speed1 = 130;
+/*int speed1 = 130;
 int speed2 = 160;
 int speed3 = 200;
-int speed4 = 255;
-int currentSpeed = speed4;
+int speed4 = 255;*/
+int currentSpeed = 255;
 
 int leftWheel = 0;
 int rightWheel = 1;
@@ -25,8 +28,8 @@ int rightWheel = 1;
 int forwardDirection = 1;
 int backwardDirection = 0;
 
-char ssid[] = "Tinchox";
-char pass[] = "22334455";
+char ssid[] = "Kuroki";
+char pass[] = "nateflau";
 int status = WL_IDLE_STATUS;
 int receivedConfig = 0;
 WiFiEspClient client;
@@ -99,23 +102,28 @@ void doTests() {
 }
 
 void fetchConfig() {
-  int params[] = {0, 
-                2, 0, 0,
-                0, 8, 0, 
-                0, 1, 5 
-        };
-  /*while (receivedConfig == 0) {
-    for (int i = 0; client.available(); i++) {
-      params[i] = client.read() - 48;
+  int params[] = {0}; //{0, 2, 5, 5, 2, 5, 0, 0, 1, 0};
+  String response = "";
+  Serial.println("fetchConfig called");
+  while (receivedConfig == 0) {
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+      response += String(c);
       receivedConfig = 1;
     }
-  }*/
-  testMode = params[0];
-  currentSpeed = params[1] * 100 + params[2] * 10 + params[3];
-  turnDelay =  params[4] * 100 + params[5] * 10 + params[6];
-  distanceThreshold = params[7] * 100 + params[8] * 10 + params[9];
+  }
+  Serial.println("***");
+  int index = response.indexOf("\r\n\r\n");
+  response = response.substring(index); // skip header
+  response.trim();
+  Serial.println(response);
+  operationMode = String(response[0]).toInt();
+  currentSpeed = String(response[1]).toInt() * 100 + String(response[2]).toInt() * 10 + String(response[3]).toInt();
+  turnDelay =  String(response[4]).toInt() * 100 + String(response[5]).toInt() * 10 + String(response[6]).toInt();
+  distanceThreshold = String(response[7]).toInt() * 100 + String(response[8]).toInt() * 10 + String(response[9]).toInt();
   
-  Serial.println(testMode);
+  Serial.println(operationMode);
   Serial.println(currentSpeed);
   Serial.println(turnDelay);
   Serial.println(distanceThreshold);
@@ -152,11 +160,11 @@ void initWifiModule() {
   Serial.println();
   Serial.println("Starting connection to server...");
   // if you get a connection, report back via serial
-  if (client.connect("arduino.cc", 80)) {
+  if (client.connect("se2019nos.nixi.icu", 80)) {
     Serial.println("Connected to server");
     // Make a HTTP request
-    client.println("GET /asciilogo.txt HTTP/1.1");
-    client.println("Host: arduino.cc");
+    client.println("GET / HTTP/1.1");
+    client.println("Host: se2019nos.nixi.icu");
     client.println("Connection: close");
     client.println();
   }
